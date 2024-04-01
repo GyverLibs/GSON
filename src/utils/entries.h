4b7b05241624b321f33858abb73f3a780238d694
GyverLibs <alexgyvershow@ya.ru>
2024-04-01 18:20:31 +0300

    upd

--- src/utils/entries.h
+++ src/utils/entries.h
@@ -1,23 +1,51 @@
 #pragma once
 #include <Arduino.h>
 #include <StringUtils.h>
+#include <limits.h>
 
 #include "types.h"
 
-#define GSON_MAX_LEN 0xffff
+namespace gson {
+#ifdef GSON_NO_LIMITS
+#define GSON_MAX_KEY_LEN 256
+#if (UINT_MAX == UINT32_MAX)
+#define GSON_PARENT_BIT 16
+typedef uint16_t parent_t;
+#else
+#define GSON_PARENT_BIT 8
+typedef uint8_t parent_t;
+#endif
+
+#else  // GSON_NO_LIMITS
 #define GSON_MAX_KEY_LEN 32
+#if (UINT_MAX == UINT32_MAX)
+#define GSON_PARENT_BIT 9
+typedef uint16_t parent_t;
+#else
+#define GSON_PARENT_BIT 8
+typedef uint8_t parent_t;
+#endif
+
+#endif  // GSON_NO_LIMITS
+}  // namespace gson
+
+#define GSON_MAX_LEN 0xffff
+#define GSON_MAX_INDEX ((1 << GSON_PARENT_BIT) - 1)
 
 namespace gsutil {
 
 struct Entry_t {
-#if (UINT_MAX == UINT32_MAX)
-    gson::parent_t parent : 9;
-#else
-    gson::parent_t parent;
-#endif
-    gson::Type type : 3;
-    uint16_t key_len : 5;
-    uint16_t val_len : 15;
+#ifdef GSON_NO_LIMITS
+    gson::parent_t parent;  // 16/8 bit
+    gson::Type type;        // 8 bit
+    uint8_t key_len;        // 8 bit
+    uint16_t val_len;       // 16 bit
+#else                       // GSON_NO_LIMITS
+    gson::parent_t parent : GSON_PARENT_BIT;  // 512/256
+    gson::Type type : 3;                      // 8
+    uint8_t key_len : 5;                      // 32
+    uint16_t val_len : 15;                    // 32 768
+#endif                      // GSON_NO_LIMITS
 
     uint16_t key_offs;
     uint16_t val_offs;
--- src/utils/types.h
+++ src/utils/types.h
@@ -1,17 +1,7 @@
 #pragma once
 #include <Arduino.h>
-#include <limits.h>
 
 namespace gson {
-
-#if (UINT_MAX == UINT32_MAX)
-typedef uint16_t parent_t;
-#define GSON_MAX_INDEX 0x1ff
-#else
-// 16 бит адрес не имеет смысла на слабых платформах
-typedef uint8_t parent_t;
-#define GSON_MAX_INDEX 0xff
-#endif
 
 enum class Type : uint8_t {
     None,
